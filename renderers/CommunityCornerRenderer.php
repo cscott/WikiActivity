9dc133e15c08dbbd92262a362fcd4944228ca058
James Haley <haleyjd@hotmail.com>
2018-03-22 01:38:44 -0500

    Code and styles for community corner rail module

---
+++ renderers/CommunityCornerRenderer.php
@@ -0,0 +1,18 @@
+<?php
+
+class CommunityCornerRenderer extends FeedRenderer {
+	public function __construct() {
+		parent::__construct('community-corner');
+	}
+
+	public function render($data, $wrap = true, $parameters = array()) {
+		$isAdmin = $wgUser->isAllowed('editinterface');
+		$this->template->set('data', $data);
+		$this->template->set('isAdmin', $isAdmin);
+		$content = $this->template->render('community.corner');
+		if ( $wrap ) {
+			$content = $this->wrap($content, false);
+		}
+		return $content;
+	}
+}
---
+++ templates/community.corner.tmpl.php
@@ -0,0 +1,8 @@
+<section class="CommunityCornerModule module">
+	<h2 class="dark_text_2"><?=wfMessage('myhome-community-corner-header')->text()?></h2>
+	<div id="myhome-community-corner-content"><?=wfMessage('community-corner')->inContentLanguage()->parse()?></div>
+
+<?php if ( $isAdmin ) { ?>
+	<div id="myhome-community-corner-edit"><a class="more" href="<?=Title::newFromText('community-corner', NS_MEDIAWIKI)->getLocalURL('action=edit')?> rel="nofollow"><?=wfMessage('oasis-myhome-community-corner-edit')->text()?></a></div>
+<?php } ?>
+</section>
