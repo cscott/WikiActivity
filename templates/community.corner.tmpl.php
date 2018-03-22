99afc65e5de5920c161f4514c34259ed7fe7bc9a
James Haley <haleyjd@hotmail.com>
2018-03-22 02:01:02 -0500

    Fix an HTML error

--- templates/community.corner.tmpl.php
+++ templates/community.corner.tmpl.php
@@ -3,6 +3,6 @@
 	<div id="myhome-community-corner-content"><?=wfMessage('community-corner')->inContentLanguage()->parse()?></div>
 
 <?php if ( $isAdmin ) { ?>
-	<div id="myhome-community-corner-edit"><a class="more" href="<?=Title::newFromText('community-corner', NS_MEDIAWIKI)->getLocalURL('action=edit')?> rel="nofollow"><?=wfMessage('oasis-myhome-community-corner-edit')->text()?></a></div>
+	<div id="myhome-community-corner-edit"><a class="more" href="<?=Title::newFromText('community-corner', NS_MEDIAWIKI)->getLocalURL('action=edit')?>" rel="nofollow"><?=wfMessage('oasis-myhome-community-corner-edit')->text()?></a></div>
 <?php } ?>
 </section>
